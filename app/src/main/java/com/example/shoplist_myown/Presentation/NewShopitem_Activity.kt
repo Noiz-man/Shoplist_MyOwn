6f58386269dcf48fcb19ae747bdc14455d99ce6a
Noiz-man <denis.ste@mail.ru>
2022-10-27 16:52:08 +0500

    Добавил в манифест строку, которая уменьшает экран при вызове экранной клавиатуры. Создал макет второго экрана

---
+++ app/src/main/java/com/example/shoplist_myown/Presentation/NewShopitem_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.shoplist_myown.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoplist_myown.R
+
+class NewShopitem_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_shopitem)
+    }
+}
