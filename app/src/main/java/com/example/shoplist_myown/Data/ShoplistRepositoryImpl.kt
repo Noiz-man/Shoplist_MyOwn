3ad353a6eecf35b7e4dabfec6cff95fa70d3379c
Noiz-man <denis.ste@mail.ru>
2022-12-21 15:33:30 +0500

    Добавил класс Mapper и изменил реализацию репозитория для работы с DB, кроме метода получения списка в livdata

--- app/src/main/java/com/example/shoplist_myown/Data/ShoplistRepositoryImpl.kt
+++ app/src/main/java/com/example/shoplist_myown/Data/ShoplistRepositoryImpl.kt
@@ -1,51 +1,34 @@
 package com.example.shoplist_myown.Data
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.shoplist_myown.Domain.Shopitem
 import com.example.shoplist_myown.Domain.ShoplistRepositiry
 import kotlin.random.Random
 
-object ShoplistRepositoryImpl: ShoplistRepositiry {
-    val shoplist = sortedSetOf<Shopitem>({o1, o2 -> o1.id.compareTo(o2.id)})
-    val shoplist_LD = MutableLiveData<List<Shopitem>>()
-    var autoincrement = 0
+class ShoplistRepositoryImpl(application: Application): ShoplistRepositiry {
 
-    init {
-        for (i in 0..99) {
-            val item = Shopitem("Имя $i", i, Random.nextBoolean())
-            addShopitem(item)
-        }
-    }
+    val shoplistDAO = AppDatabase.getInstance(application).shopListDao()
+    val mapper = Mapper()
 
     override fun addShopitem(item: Shopitem) {
-        if (item.id == Shopitem.UNDEFINED_ID) {
-            item.id = autoincrement++
-        }
-        shoplist.add(item)
-        updateShoplist()
+        shoplistDAO.addShopItemDb(mapper.mapEntityToShopitemDB(item))
     }
 
     override fun deleteShopitem(item: Shopitem) {
-        shoplist.remove(item)
-        updateShoplist()
+        shoplistDAO.deleteShopItemDb(item.id)
     }
 
     override fun editShopitem(item: Shopitem) {
-        val olditem = getShopitemByID(item.id)
-        shoplist.remove(olditem)
-        addShopitem(item)
+        shoplistDAO.addShopItemDb(mapper.mapEntityToShopitemDB(item))
     }
 
     override fun getShopitemByID(id: Int): Shopitem {
-        return shoplist.find { it.id == id } ?: throw RuntimeException("Нет такого ID")
+        return mapper.mapShopitemDBtoEntity(shoplistDAO.getShopItemByID(id))
     }
 
     override fun getShoplist(): LiveData<List<Shopitem>> {
-        return shoplist_LD
-    }
-
-    fun updateShoplist() {
-        shoplist_LD.value = shoplist.toList()
+        return mapper.mapListDBToListEntity()
     }
 }
